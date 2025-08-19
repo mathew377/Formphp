e2305d499d0ecff997c5b97182eb01766d77e252
mathew377 <mathew.avendano10@gmail.com>
2025-08-19 16:44:05 -0500

    Update productos.php

--- productos.php
+++ productos.php
@@ -1,1 +1,23 @@
+<?php
+$Producto1 = $_POST['Producto1'];
+$Producto2 = $_POST['Producto2'];
+$Producto3 = $_POST['Producto3'];
+$PrecioP1 = $_POST['PrecioP1'];
+$PrecioP2 = $_POST['PrecioP2'];
+$PrecioP3 = $_POST['PrecioP3'];
+$SumaProductos = PrecioP1+PrecioP2+PrecioP3;
+$Descuento= SumaProductos*0.16;
+$Total= SumaProductos-Descuento;
 
+
+
+
+
+echo "El producto numero 1 es: " . $Producto1 ;
+echo "El producto numero 2 es: " . $Producto2 ;
+echo "El producto numero 3 es: " . $Producto3 ;
+echo "La Suma de los productos es de: " . $SumaProductos;
+echo "El Desucento es de: " . $Decuento;
+echo "El Total a pagar es de: " . $Total;
+
+?>
